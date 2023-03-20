e007d0a51b7ee7e816b65d5eaeebfb2464278f90
AhmedRajaa <ahmedrajaa2021@gmail.com>
2023-03-20 16:32:55 +0200

    Initial commit

---
+++ app/src/main/java/com/example/assignment2/uploadPdf.java
@@ -0,0 +1,156 @@
+package com.example.assignment2;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DownloadManager;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import static android.os.Environment.DIRECTORY_DOWNLOADS;
+
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.Objects;
+
+public class uploadPdf extends AppCompatActivity {
+    Button button_upload;
+    Button button_viewFilePdf;
+    EditText pdf_name;
+    DatabaseReference databaseReference;
+    StorageReference storageReference;
+    FirebaseStorage firebaseStorage;
+    StorageReference reference;
+
+    Button button_download;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_pdf);
+        button_upload = findViewById(R.id.buttonUpload);
+        pdf_name = findViewById(R.id.textViewPdf);
+        button_download = findViewById(R.id.buttonDownload);
+        button_viewFilePdf = findViewById(R.id.buttonViewPdf);
+        storageReference = FirebaseStorage.getInstance().getReference();
+        databaseReference = FirebaseDatabase.getInstance().getReference("Uploads");
+        button_upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectFile();
+            }
+        });
+
+    }
+
+    private void selectFile() {
+        Intent intent = new Intent();
+        intent.setType("application/pdf");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Pdf File ..."), 1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            UploadFiles(data.getData());
+        }
+    }
+
+    private void UploadFiles(Uri data) {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Uploading...");
+        progressDialog.show();
+        StorageReference reference = storageReference.child("Uploads/" + System.currentTimeMillis() + ".pdf");
+        reference.putFile(data).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                while (!uriTask.isComplete()) ;
+                Uri url = uriTask.getResult();
+                pdfClass pdfClass = new pdfClass(pdf_name.getText().toString(), url.toString());
+                databaseReference.child((databaseReference.push().getKey())).setValue(pdfClass);
+                Toast.makeText(uploadPdf.this, "File Uploaded!!", Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                double progress = (100.0 * snapshot.getBytesTransferred()) / snapshot.getTotalByteCount();
+                progressDialog.setMessage("Uploaded:" + (int) progress + "%");
+
+            }
+        });
+        button_viewFilePdf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button_download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                download();
+
+            }
+        });
+    }
+
+    public void download()
+    {
+        storageReference= FirebaseStorage.getInstance().getReference();
+        StorageReference ref=storageReference.child("mobile.pdf");
+
+        ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                String url=uri.toString();
+                downloadFile(uploadPdf.this,"Mobile",".pdf",DIRECTORY_DOWNLOADS,url);
+
+            }
+        });
+    }
+
+    public void downloadFile(Context context, String fileName, String fileExtension, String destinationDirectory, String url) {
+
+        DownloadManager downloadmanager = (DownloadManager) context.
+                getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(url);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalFilesDir(context, destinationDirectory, fileName + fileExtension);
+
+        downloadmanager.enqueue(request);
+    }
+}
+
+
